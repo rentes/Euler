c0a9c73740c67e4748bfe0aa8edb14b2be44378b
Miguel Rentes <miguel.rentes@gmail.com>
2012-04-16 23:31:09 +0100

    moved all variables together to the top

--- problem2.py
+++ problem2.py
@@ -5,6 +5,11 @@
 
 # fibonacci dictionary used for memoization
 fibs = {0:0, 1:1}
+# counter that will hold the sum
+count = 0
+# counter for iterations
+n = 1
+
 # fibonacci definition
 # based on Dijsktra's paper here: www.cs.utexas.edu/users/EWD/ewd06xx/EWD654.PDF
 # love it
@@ -16,10 +21,7 @@ def fib(n):
     else:
         fibs[n] = (fib((n-1)/2) ** 2) + (fib((n+1)/2) ** 2)
         return fibs[n]
-# counter that will hold the sum
-count = 0
-# counter for iterations
-n = 1
+
 while count < 4000000:
     if fib(n) % 2 == 0: # we only want even fibonacci numbers
         count = count + fib(n) # add it to the sum
