8046fcb352ca8ba4ae30bd04436fcf87a1fe40fd
Miguel Rentes <miguel.rentes@gmail.com>
2012-04-19 21:43:01 +0100

    Adding solution for Problem 3

---
+++ problem3.py
@@ -0,0 +1,37 @@
+# Project Euler - Problem 3
+# http://projecteuler.net/problem=3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+# My idea to solve this problem:
+# 1 - discover all factors (prime or non-prime) of n
+# 2 - as we discover new factors, multiply them all to see if they equal n
+# 3 - if they equal n, we found all factors and only need to obtain the max
+# 4 - if not equal to n, continue search for factors until reaching n
+
+def multiplyFactors(l):
+    v = 1
+    for x in l:
+        v *= x
+    return v
+
+def factors(n):
+    # list with factors (prime or non-prime) of number n
+    l = []
+    # first number to divide n
+    m = 2
+    # while all factors multiplied don't equal n and m is not n yet
+    while n != multiplyFactors(l) and m <= n:
+        if n % m == 0:
+            # factor found. let's add it to the list
+            l.append(m)
+            # sort the list for easy retrieval of last element
+            l.sort()
+        # continue factor search
+        m += 1
+    # uncomment next line to see list of discovered factors
+    # print l
+    return l
+
+# finally, print the last value of list of factors (we sorted the list)
+print factors(600851475143)[-1]
