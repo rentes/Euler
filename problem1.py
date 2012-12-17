d96f19cf8cef9351c0cb6aef63d69da0ba0e20c5
Miguel Rentes <miguel@CPT00752.(none)>
2012-12-17 18:31:41 +0000

    Corrected solution to conform with PEP8

--- problem1.py
+++ problem1.py
@@ -6,6 +6,7 @@ Add all the natural numbers below one thousand that are multiples of 3 or 5.
 
 import time
 
+
 def sum_numbers():
     """
     Sums all natural numbers below 1000 that are multiples of 3 or 5
@@ -13,15 +14,16 @@ def sum_numbers():
     """
     a, b = 1, 0
     while a < 1000:
-        if ((a % 3 == 0) or (a % 5 == 0)): # if a is multiple or 3 or 5
-            b += a # add a to the sum
-        a += 1 # let us check next integer
+        if ((a % 3 == 0) or (a % 5 == 0)):  # if a is multiple or 3 or 5
+            b += a  # add a to the sum
+        a += 1  # let us check next integer
     return b
 
 start = time.time()
 print(sum_numbers())
 elapsed = (time.time() - start)
-print ('time:', elapsed, 'seconds ~', \
-    int(elapsed/60), 'minutes,', \
-    int(elapsed)%60, 'seconds,', \
-    int(elapsed*1000)-((int(elapsed)%60)*1000), 'milliseconds')
+print('time for this problem to complete:')
+print(elapsed, 'seconds ~')
+print(int(elapsed / 60), 'minutes,')
+print(int(elapsed) % 60, 'seconds,',)
+print(int(elapsed * 1000) - ((int(elapsed) % 60) * 1000), 'milliseconds')
