a0502fc36739ff1bbc1a2b1d02428176f68d1343
Miguel Rentes <miguel@CPT00752.(none)>
2012-12-18 16:13:28 +0000

    Corrected calculation for ms on elapsed_time function

--- problem4.py
+++ problem4.py
@@ -86,7 +86,7 @@ def elapsed_time(elapsed):
     """
     min = int(elapsed / 60)  # minutes
     s = int(elapsed) % 60  # seconds
-    ms = int(elapsed * 1000) - ((int(elapsed) % 60) * 1000)  # milliseconds
+    ms = int((elapsed - int(elapsed)) * 1000)  # milliseconds
     print('time:', elapsed, 's ~', min, 'min,', s, 's,', ms, 'ms')
 
 start = time.time()
