5bdb5c189de7d792b1bf087a2aabbec47322d3f1
Miguel Rentes <miguel.rentes@gmail.com>
2014-02-05 15:34:57 +0000

    refactoring code

--- problem7.py
+++ problem7.py
@@ -11,23 +11,32 @@ def is_prime(n):
     """
     if n < 2:
         return False
-    if n == 2:  # the first prime number
+    # the first prime number
+    if n == 2:
         return True
-    if n % 2 == 0:  # all even numbers, except 2, are not prime numbers
+    # all even numbers, except 2, are not prime numbers
+    if n % 2 == 0:
         return False
-    for x in range(3, int(n ** 0.5) + 1, 2):  # primality test
-        if n % x == 0:  # dividing n for all numbers in range [3, sqrt(n) + 1]
-            return False  # n is disible by x, so n is not a prime
-    return True  # if we get here, than n is a prime
+    # primality test
+    for x in range(3, int(n ** 0.5) + 1, 2):
+        # dividing n for all numbers in range [3, sqrt(n) + 1]
+        if n % x == 0:
+            # n is disible by x, so n is not a prime
+            return False
+    # if we get here, than n is a prime
+    return True
 
 
 def ten_thousand_first_prime():
     """
     Finds the 10 001st prime number
     """
-    n = 2  # natural numbers, starting with 2
-    count = 0  # prime number counter
-    largest = 0  # the newest prime number found
+    # natural numbers, starting with 2
+    n = 2
+    # prime number counter
+    count = 0
+    # the newest prime number found
+    largest = 0
     while count < 10001:
         if is_prime(n) is True:
             count += 1
