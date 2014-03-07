7d80a65e56cbd4d3aff8b9bc3769e71dd5d60d95
Miguel Rentes <miguel.rentes@gmail.com>
2014-03-07 11:00:44 +0000

    correcting the minutes calculation

--- tools/timeutils.py
+++ tools/timeutils.py
@@ -12,7 +12,7 @@ def elapsed_time(elapsed):
     Computes the amount of time spent by the algorithm and outputs the time
     """
     hr = int(elapsed / 3600)  # hours
-    min = int(elapsed / 60)  # minutes
+    min = int((elapsed % 3600) / 60)  # minutes
     s = int(elapsed) % 60  # seconds
     ms = int((elapsed - int(elapsed)) * 1000)  # milliseconds
     print('time:', elapsed, 's ~', hr, 'hour,', min, 'min,', s, 's,', ms, 'ms')
