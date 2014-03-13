2d135a88bde1e113e344df5469494df1c7fc00ee
Miguel Rentes <miguel.rentes@gmail.com>
2014-03-13 12:12:21 +0000

    Added a descriptive comment on the algorithm to find the number of factors of number n

--- problem12.py
+++ problem12.py
@@ -7,23 +7,32 @@ import tools.timeutils as timeUtils
 def number_of_factors(n):
     """
     Returns the number of factors of number n
-    Note: not the actual factors, just how many there are
+
+    Using a list to keep the factors found of number n
     """
-    factors = 0
     factor_found = 0
     max_limit = 0
     list = []
-    list.append(1)
+    list.append(1)  # 1 is always a factor
 
     for m in range(2, n):
         if n % m == 0:
+            # found a new factor
             factor_found = int(n / m)
+            # I only have to divide n by m until m reaches the result of
+            # the quotient of the first factor encountered
+            # for example: consider number 28. the first factor is 2 and
+            # the quotient gives 14, since 28 / 2 = 14. 14 is then the max
+            # limit where m has to increase to, because we know for sure that
+            # any m > 14 will not be a factor of 28, and we break the cycle
+            # when this condition occurs. This way we only have to make less
+            # divisions to find out all the factors of number n
             if max_limit < factor_found:
                 max_limit = factor_found
             list.append(factor_found)
         if m > max_limit:
             break
-    list.append(n)
+    list.append(n)  # number n is also always a factor
     return len(list)
 
 
