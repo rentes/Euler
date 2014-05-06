25a58d12bf178ec879650d28b29f15e80c6b0b9c
Miguel Rentes <miguel.rentes@gmail.com>
2014-05-06 18:02:50 +0100

    Problem 15 solution

---
+++ problem15.py
@@ -0,0 +1,24 @@
+"""Project Euler - Problem 15 - http://projecteuler.net/problem=15"""
+import sys
+import time
+import math
+import tools.timeutils as timeUtils
+
+
+def combinations_n_k(n, k):
+    """Returns the number of combinations n choose k"""
+    return int(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))
+
+
+def main():
+    """Main entry point for the script"""
+    start = time.time()
+
+    # please see http://copingwithcomputers.com/2013/07/06/lattice-paths/
+    # for an explanation on the relation of lattice paths with Pascal numbers
+    print(combinations_n_k(40, 20))
+
+    timeUtils.elapsed_time(time.time() - start)
+
+if __name__ == '__main__':
+    sys.exit(main())
