d466b9312cd7a02f2c2866b6e99d4cf4a0b40a71
Miguel Rentes <miguel.rentes@gmail.com>
2014-05-06 18:27:45 +0100

    Problem 16 solution

---
+++ problem16.py
@@ -0,0 +1,21 @@
+"""Project Euler - Problem 16 - http://projecteuler.net/problem=16"""
+import sys
+import time
+import math
+import tools.timeutils as timeUtils
+
+
+def main():
+    """Main entry point for the script"""
+    start = time.time()
+
+    # short explanation:
+    # str(number) - converts number to string
+    # map(int,string) - converts each charater to int
+    # sum(list) - sums the list of ints (in this case)
+    print(sum(map(int,str(int(math.pow(2, 1000))))))
+
+    timeUtils.elapsed_time(time.time() - start)
+
+if __name__ == '__main__':
+    sys.exit(main())
