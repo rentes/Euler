1cb90441bfb27419620e6d593126b359e425f2b2
Miguel Rentes <miguel.rentes@gmail.com>
2014-05-06 15:27:30 +0100

    Problem 13 solution

---
+++ problem13.py
@@ -0,0 +1,19 @@
+"""Project Euler - Problem 13 - http://projecteuler.net/problem=13"""
+import sys
+import time
+import tools.timeutils as timeUtils
+
+
+def main():
+    """Main entry point for the script"""
+    start = time.time()
+    a = 0
+    with open('problem13-data.txt') as f:
+        for line in f:
+            b = int(line.strip())
+            a += b
+    print(str(a)[:10])
+    timeUtils.elapsed_time(time.time() - start)
+
+if __name__ == '__main__':
+    sys.exit(main())
