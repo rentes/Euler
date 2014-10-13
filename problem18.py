e76e5e1c544c5f3f90202b79dc2a6b3db0bdee9e
Miguel Rentes <miguel.rentes@gmail.com>
2014-10-13 12:55:22 +0100

    Starting solution for problem 18

---
+++ problem18.py
@@ -0,0 +1,16 @@
+"""Project Euler - Problem 18 - http://projecteuler.net/problem=18"""
+import sys
+import time
+import tools.timeutils as timeUtils
+
+
+def main():
+    """Main entry point for the script"""
+    start = time.time()
+
+    print("test")
+
+    timeUtils.elapsed_time(time.time() - start)
+
+if __name__ == '__main__':
+    sys.exit(main())
