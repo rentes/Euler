381534c0efc255e7065c0b0bbe0eb3fbce0ed6e7
Miguel Rentes <miguel.rentes@gmail.com>
2015-01-07 11:21:11 +0000

    Small tweaks on the python .gitignore and __init__.py files addition

--- problem18.py
+++ problem18.py
@@ -1,16 +1,15 @@
 """Project Euler - Problem 18 - http://projecteuler.net/problem=18"""
 import sys
 import time
-import tools.timeutils as timeUtils
+from tools import timeutils
 
 
 def main():
-    """Main entry point for the script"""
     start = time.time()
 
     print("test")
 
-    timeUtils.elapsed_time(time.time() - start)
+    timeutils.elapsed_time(time.time() - start)
 
 if __name__ == '__main__':
     sys.exit(main())
--- tools/timeutils.py
+++ tools/timeutils.py
@@ -4,15 +4,13 @@ a given Euler problem took to run
 Author: Miguel Rentes
 """
 
-import time
-
 
 def elapsed_time(elapsed):
     """
     Computes the amount of time spent by the algorithm and outputs the time
     """
-    hr = int(elapsed / 3600)  # hours
-    min = int((elapsed % 3600) / 60)  # minutes
-    s = int(elapsed) % 60  # seconds
-    ms = int((elapsed - int(elapsed)) * 1000)  # milliseconds
-    print('time:', elapsed, 's ~', hr, 'hour,', min, 'min,', s, 's,', ms, 'ms')
+    hours = int(elapsed / 3600)  # hours
+    minutes = int((elapsed % 3600) / 60)  # minutes
+    seconds = int(elapsed) % 60  # seconds
+    milliseconds = int((elapsed - int(elapsed)) * 1000)  # milliseconds
+    print('time:', elapsed, 's ~', hours, 'hour,', minutes, 'min,', seconds, 's,', milliseconds, 'ms')
